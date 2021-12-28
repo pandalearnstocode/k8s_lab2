c16cc106f88c1b2f1d4f5fbe283d46039db8da79
Aritra Biswas <pandalearnstocode@gmail.com>
2021-12-28 11:33:46 +0530

    user logged in successful.

---
+++ toy_apps/frontend/app/main.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import requests
+import json
+url = 'http://web:8000/login'
+
+application_form = st.form(key='application-layer')
+application_email = application_form.text_input('Enter your email')
+application_password = application_form.text_input('Enter your password')
+application_submit = application_form.form_submit_button('Submit')
+
+if application_submit:
+    login_data = {"email": application_email,"password":application_password}
+    r = requests.post(url, data=json.dumps(login_data))
+    st.write(f'hello {r.text}')
