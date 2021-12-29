77c98d7b18d492d44889b1fe4d822af1b334872e
Aritra Biswas <pandalearnstocode@gmail.com>
2021-12-29 12:12:09 +0530

    trying to get basic lib structure working.

--- toy_apps/ms/data_lib/setup.py
+++ toy_apps/ms/data_lib/setup.py
@@ -1,16 +1,17 @@
 from setuptools import find_packages, setup
-from core import __version__
 
 
 setup(name="datalib",
       keywords="datalib",
-      packages=find_packages(include=["core", "core.*"]),
+      packages=find_packages(),
       install_requires=["psycopg2-binary==2.9.1", "pandas","SQLAlchemy","loguru","typer","typer-cli"],
-      version=__version__,
+      version="0.0.1",
       description="A data utility",
       author="Aritra Biswas",
       author_email='pandalearnstocode@gmail.com',
       platforms=["any"],
       license="BSD",
+      py_modules=["datalib"],
+      package_dir={'':'quicksample/src'},
       url="http://github.com/pandalearnstocode/datalib"
 )
